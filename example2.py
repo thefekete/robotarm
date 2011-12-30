8361e5aabc91c36a51ca7737f7a53c744749da4c
Dan Fekete <thefekete@gmail.com>
2011-12-29 23:02:59 -0800

    added some prompting to examples

--- example2.py
+++ example2.py
@@ -2,11 +2,11 @@ from robotarm.al5x import Al5x, AL5D
 from robotarm.ssc32 import Ssc32
 
 if __name__ == '__main__':
-
     a = Al5x(AL5D, parked_state=dict(pos=(0,10,2.6), grip=-.4),
              dt=0.010, avg_speed=15,
              servo_controller=Ssc32('/dev/ttyUSB0'))
     try:
+        raw_input("Press Enter to continue, ctrl-C to stop")
         c = 0
         while True:
             a.move(dict(pos=(-1,8,6+(c%5)), grip_angle=0.0))
